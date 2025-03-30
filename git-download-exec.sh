c86bc143b9aff5535a2b2c5c3b6edb8c812b4832
SophieGrig <48683427+SophieGrig@users.noreply.github.com>
2025-03-30 13:25:45 +0300

    Update git-download-exec.sh

--- git-download-exec.sh
+++ git-download-exec.sh
@@ -2,7 +2,7 @@
 
 # Define the repository URL and target directory
 REPO_URL="https://github.com/SophieGrig/linux-backup.git"
-TARGET_DIR="$HOME/custom-commands"
+TARGET_DIR="$HOME/linux-backup"
 
 # Clone the repository into the target directory
 git clone "$REPO_URL" "$TARGET_DIR" || { echo "Failed to clone the repository."; exit 1; }
